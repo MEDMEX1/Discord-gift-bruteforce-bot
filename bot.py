c6211c7784b8c436196bae38ffb2b7214bc35671
MEDMEX1 <44372364+MEDMEX1@users.noreply.github.com>
2020-04-06 23:03:17 +0200

    Update bot.py
    Fixed the bot, added random delay

--- bot.py
+++ bot.py
@@ -1,26 +1,32 @@
 import discord
 from discord.ext import commands
 import asyncio
-import random
+import random as r
 import string
+
+#Gift bruteforce bot by MEDMEX
 
 client = commands.Bot(command_prefix='$')
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=discord.Game(name='Genning nitro codes', type=0))
     print("ready")
 
 
 
 @client.command(pass_context=True)
 async def start(ctx):
-    code = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
-    await client.send_message(ctx.message.channel, "https://discord.gift/" + code)
-    return code
+    var = 1
+    while var == 1 :
+        code = ''.join(r.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
+        channel = ctx.message.channel
+        await channel.send("https://discord.gift/" + code)
+        await asyncio.sleep(r.randint(2,5))
 
 
 @client.command(pass_context=True)
 async def test(ctx):
     channel = ctx.message.channel
     await client.send_message(channel, "https://discord.gift/" + "rhlRY2HnxMk2JDAK")
+
+client.run(")
